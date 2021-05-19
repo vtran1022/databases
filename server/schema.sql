647defcf793386353924c0b2dd5968642b1f238f
Joey Rivera <josephrivera5000@gmail.com>
2021-05-19 16:09:53 -0700

    First pass at implementing a chatterbox database schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,39 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  time_stamp Datetime NOT NULL,
+  message_text TEXT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (room_id) REFERENCES rooms (id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  username VARCHAR(110) NOT NULL,
+  fullname VARCHAR(110) NOT NULL,
+  email VARCHAR(110) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE rooms (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(110) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-
+CREATE TABLE room_subscriptions (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (room_id) REFERENCES rooms (id)
+)
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
