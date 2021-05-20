8b45b80f2dbe9d0310cddc19ade00197d460895d
Joey Rivera <josephrivera5000@gmail.com>
2021-05-19 17:28:39 -0700

    Working on it

--- server/schema.sql
+++ server/schema.sql
@@ -2,41 +2,86 @@ CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS `messages`;
+
+/* Describe your table here.*/
 CREATE TABLE messages (
-  /* Describe your table here.*/
   id INT unsigned NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
-  room_id INT NOT NULL,
-  time_stamp Datetime NOT NULL,
-  message_text TEXT NULL,
+  id_user INT NOT NULL,
+  id_room INT NOT NULL,
+  time_stamp DATETIME NOT NULL,
+  message_text MEDIUMTEXT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (room_id) REFERENCES rooms (id)
+  FOREIGN KEY (id_user) REFERENCES users (id),
+  FOREIGN KEY (id_room) REFERENCES rooms (id)
 );
+
+/*
+CREATE TABLE `messages` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user` INTEGER NOT NULL,
+  `room_id` INT NOT NULL,
+  `time_stamp` DATETIME NULL,
+  `message_text` MEDIUMTEXT NULL DEFAULT NULL,
+  `id_users` INT NOT NULL DEFAULT NULL,
+  `id_rooms` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+*/
+DROP TABLE IF EXISTS `users`;
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE users (
   id INT unsigned NOT NULL AUTO_INCREMENT,
   username VARCHAR(110) NOT NULL,
-  fullname VARCHAR(110) NOT NULL,
-  email VARCHAR(110) NOT NULL,
   PRIMARY KEY (id)
 );
+
+/*
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(110) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+*/
+
+DROP TABLE IF EXISTS `rooms`;
 
 CREATE TABLE rooms (
   id INT unsigned NOT NULL AUTO_INCREMENT,
-  room_name VARCHAR(110) NOT NULL,
+  room_name VARCHAR(110) NULL DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
+/*
+CREATE TABLE `rooms` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `room_name` VARCHAR(110) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+*/
+
+DROP TABLE IF EXISTS `room_subscriptions`;
+
 CREATE TABLE room_subscriptions (
   id INT unsigned NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
-  room_id INT NOT NULL,
+  id_user INT NOT NULL,
+  id_room INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (room_id) REFERENCES rooms (id)
-)
+  FOREIGN KEY (id_user) REFERENCES users (id),
+  FOREIGN KEY (id_room) REFERENCES rooms (id)
+);
+
+/*
+CREATE TABLE `room_subscriptions` (
+  `id` INT NOT NULL AUTO_INCREMENT DEFAULT 0,
+  `user` INTEGER NOT NULL,
+  `room_id` INTEGER NULL DEFAULT NULL,
+  `id_rooms` INT NOT NULL,
+  `id_users` INT NOT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+*/
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
